9fa28bcdfbcae1574971169e79d140b287328cb9
Umair Tahir <professor.ut.786@gmail.com>
2019-01-27 14:13:17 +0500

    Map creation and location marker made

---
+++ app/src/main/java/ut786/clone/AndroidUberClone/Models/User.java
@@ -0,0 +1,47 @@
+package ut786.clone.AndroidUberClone.Models;
+
+public class User {
+    private String name, email, password, phone;
+
+    public User() {
+    }
+
+    public User(String name, String email, String password, String phone) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
